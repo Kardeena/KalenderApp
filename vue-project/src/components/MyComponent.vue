0d2b6e7cab44b00d528ba99a39f4449adb3f9097
Kardeena Kameran <kardeenakameran2@gmail.com>
2024-05-12 23:53:48 +0200

    Vue-Komponente erstellt

---
+++ vue-project/src/components/MyComponent.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
+<template>
+  <div>
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyComponent',
+  data() {
+    return {
+      items: [{name: 'Item 1'}, {name: 'Item 2'}],
+      isLoggedIn: true
+    };
+  }
+}
+</script>
+
+<style scoped>
+/* CSS für deine Komponente */
+</style>
